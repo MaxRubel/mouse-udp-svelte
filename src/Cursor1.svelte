63989571ef577b4431a47f5a374a38c7059ec01c
Max Rubel <maxrubel1@gmail.com>
2024-05-21 22:26:40 -0500

    working

---
+++ src/Cursor1.svelte
@@ -0,0 +1,21 @@
+<script>
+    console.log("hey")
+    export let p2x
+    export let p2y
+
+</script>
+
+<main>
+<div class="cursor" style="left:{p2x}px; top: {p2y}px;">
+</div>
+</main>
+
+<style>
+.cursor{
+    height: 8px;
+    width: 8px;
+    border-radius: 100%;
+    background-color: blue;
+    position: absolute;
+}
+</style>
