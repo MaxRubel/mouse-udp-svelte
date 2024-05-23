558a92d66465ebda6c14b6ff48ce37ecffd9bf3e
Max Rubel <maxrubel1@gmail.com>
2024-05-23 18:36:55 -0500

    can choose players

---
+++ src/ChoosePlayer.svelte
@@ -0,0 +1,25 @@
+
+<script>
+    export let choosePlayerNo;
+    export let player1;
+    export let player2;
+
+</script>
+
+<main>
+    <button on:click={()=>{
+        choosePlayerNo(1)
+    }}
+    disabled={player1}
+    >
+        Player 1
+    </button>
+
+    <button on:click={()=>{
+        choosePlayerNo(2)
+    }}
+    disabled={player2}
+    >
+        Player 2
+    </button>
+</main>
